73879d5fac9a5a7e34874c84fefdcae1c79f47f1
mario <mario@uniplaces.com>
2016-01-22 01:07:29 +0000

    add some routers

--- app/router.js
+++ app/router.js
@@ -6,6 +6,15 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('home');
+  this.route('orgs', {});
+  this.route('org', {path: 'org/:id'}, function() {
+    this.route('repos', {});
+    this.route('repo', {path: ':id'}, function() {
+      this.route('contributors', {});
+      this.route('issues', {});
+    });
+  });
 });
 
 export default Router;
