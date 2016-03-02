9aaa25673e15080a3013fd3bddf495ff2d7449cb
mario <mario@uniplaces.com>
2016-03-02 00:51:01 +0000

    add more routes and redirects and also hardcoded a model

--- app/routes/orgs.js
+++ app/routes/orgs.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model() {
+		return [
+			{id: "emberjs"},
+			{id: "ember-cli"},
+			{id: "nodejs"},
+			{id: "sass"},
+			{id: "postcss"}
+		];
+	}
 });
